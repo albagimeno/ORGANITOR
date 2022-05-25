42e2e9b97d65124149a5b1750974eb1b3056021a
albagimeno <albagm09@gmail.com>
2022-05-25 17:47:05 +0200

    web

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
+const express = require('express')
+const portainer = express()
 
 const app = require('./server');
 require('./database');
 
+
 app.listen(app.get('port'), () => {
      console.log('Servidor en el puerto', app.get('port'))
 })
+
+
+// portainer.get('/', (req, res) => res.send('Hello World!'))
+portainer.listen(process.env.PORT_PORTAINER, () => console.log(`Portainer escuchando en el puerto ${process.env.PORT_PORTAINER}`))
